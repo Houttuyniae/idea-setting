e1f93807802f3820f78861ae7c23b35c74d53bb3
owl <122615922@qq.com>
2019-02-16 13:45:03 +0800

    IU-2018.3.3 <Owl@LAPTOP-I6202CTF Overwrite remote https://github.com/Houttuyniae/springboot-study.git to local

--- studyUtils/src/main/java/com/studyUtils/studyUtils/user/mapper/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.studyUtils.studyUtils.user.mapper.dao;
-
-import com.studyUtils.studyUtils.user.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-
-public interface UserDao {
-
-}
